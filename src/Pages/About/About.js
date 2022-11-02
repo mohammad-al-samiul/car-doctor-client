43aef406b18e661422bbb316ed3f7000f03a88e9
Noob-H4cker <alsamiul123@gmail.com>
2022-11-02 21:54:14 +0600

    about us complete

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,42 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from 'react';
+import parts from '../../Assets/images/about_us/parts.jpg';
+import person from '../../Assets/images/about_us/person.jpg';
+import './About.css';
 
 const About = () => {
   return (
     <div>
-      <h1>This is About</h1>
+      <div className="hero rounded-xl mt-20">
+        <div className="hero-content flex-col lg:flex-row ">
+          <div className="w-1/2 relative ">
+            <img src={person} className="w-4/5 h-full rounded-lg shadow-2xl" alt="" />
+            <img
+              src={parts}
+              className="absolute rounded-lg shadow-2xl w-3/5 right-5 top-1/2 border-8 about-image"
+              alt=""
+            />
+          </div>
+          <div className="w-1/2">
+            <div>
+              <h1 className="text-5xl font-bold my-5 pt-10">
+                <span className="text-red-500 font-bold text-2xl mb-3">About Us</span> <br />
+                We are qualified <br /> & of experience <br /> in this field
+              </h1>
+              <p className="py-4">
+                There are many variations of passages of Lorem Ipsum available, but the majority
+                have suffered alteration in some form, by injected humour, or randomised words which
+                don't look even slightly believable.
+              </p>
+              <p className="py-4 ">
+                the majority have suffered alteration in some form, by injected humour, or
+                randomised words which don't look even slightly believable.
+              </p>
+              <button className="btn btn-primary">Get More info</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
